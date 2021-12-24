e1afe11024deb1c6443d803414c836b448634e80
Shelby Potts <shelbypotts@esparklearning.com>
2021-12-24 08:18:26 -0700

    upload file work

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -1,31 +1,45 @@
+import os.path
 import boto3
 import sys
+import yaml
+
 
 def main():
-    if (len(sys.argv)  6):
-        print ('Error: Required 5 arguments.')
-        # Checks for 6 because the script path is in position 0. So len is 6
-        # for 5 arguments.
-        sys.exit(1)
+    # read config values from the yml into a dictionary
+    with open('upload_file_to_s3.yml', 'r') as config_file:
+        config_values = yaml.safe_load(config_file)
 
-    bucket_name=sys.argv[1]
-    aws_key=sys.argv[2]
-    aws_access_key=sys.argv[3]
-    aws_access_secret=sys.argv[4]
-    local_path=sys.argv[5]
+    # parse the config values into program variables
+    bucket_name = config_values['s3']['bucket_name']
+    object_path = config_values['s3']['obj_destination_path']
+    file_abs_path = sys.argv[1]
 
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key,
-        aws_secret_access_key=aws_access_secret,
+    print(f'bucket_name: {bucket_name}')
+    print(f'object_name: {object_path}')
+    print(f'file_abs_path: {file_abs_path}')
+
+    file_name = os.path.basename(file_abs_path)
+
+    # setup the s3 resource
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name='us-east-1'
     )
-    client = session.client('s3')
 
-    response = client.upload_file(
-        Filename=local_path,
-        Bucket=bucket_name,
-        Key=aws_key
-    )
-    print ('Done uploading')
+    # get the bucket from s3
+    bucket = s3.Bucket(bucket_name)
+    # get the objects in the bucket within the object path
+    objects = bucket.objects.filter(Prefix=object_path)
+    for obj in objects:
+        # if the file is not in the bucket already, put the file in s3
+        if obj.key == object_path + file_name:
+            print(f'File {file_name} already exists in the s3 bucket path {object_path}.')
+        else:
+            new_s3_object = s3.Object(bucket_name, object_path + file_name)
+            # new_s3_object.put()
+            print(f'Uploaded {file_name} to s3.')
+
+    print('Upload complete.')
 
 
 main()
--- scripts/release.sh
+++ scripts/release.sh
@@ -5,13 +5,14 @@ aws_access_secret=$4
 local_path=$5
 
 # Remove any existing versions of a ZIP
-rm -rf $local_path
+#rm -rf $local_path
 
 # Create a zip of the current directory.
-zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
+#zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
 
 # Install required dependencies for Python script.
 pip3 install boto3
+pip3 install pyyml
 
-# Run upload script
-python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
+# run upload script
+python3 pipeline/upload_file_to_s3.py "$file_abs_path"
